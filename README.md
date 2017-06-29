d28fca11e8309f2090b35b088aea84dfe1755ed1
rockhazard <rockhazardz@gmail.ocm>
2017-06-29 14:57:57 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# xtouch
+
+### What is this?
+Xtouch is an automation wrapper for the GNU touch command. It enables users to produced and arbitrary number of files with random names of a desired number of characters within the current directory.
+
+### Usage
+After ensuring the file is executable, type `xtouch.py --help`.
+
+### Requirements
+Linux
---
+++ xtouch.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+===============================================================================
+Xtouch creates a given number of random files within the parent directory.
+===============================================================================
+"""
+
+
+from textwrap import dedent
+# from pathlib import Path
+import sys
+import argparse
+import random
+import string
+from subprocess import run
+__author__ = "Ike Davis"
+
+
+def gen_random_name(filename=8, ext="txt"):
+    characters = string.printable[0:63]
+    filenameList = []
+    extList = []
+
+    for i in range(1, filename + 1):
+        filenameList.append(random.choice(characters))
+    try:
+        for i in range(1, int(ext) + 1):
+            extList.append(random.choice(characters))
+        extension = "".join(extList)
+    except ValueError:
+        extension = ext
+
+    name = "".join(filenameList)
+    randName = name + "." + extension
+    return randName
+
+
+def main(*args):
+    """
+    COMMANDLINE OPTIONS
+    """
+    parser = argparse.ArgumentParser(
+        prog=sys.argv[0][2:],
+        description=dedent("""\
+            %(prog)s creates a given number of files with random names in the 
+            parent directory."""),
+        epilog="""Author: Ike Davis\nLicense: MIT""")
+    parser.add_argument('--version', help='print version info then exit',
+                        version='%(prog)s 1.0', action='version')
+    parser.add_argument('--generate', '-g', nargs=3,
+                        metavar=('NAME_LENGTH', 'EXT_LENGTH', 'NUMBER'),
+                        help=dedent("""\
+                                    Create a NUMBER of files with random names 
+                                    of NAME_LENGTH with extension EXT_LENGTH. If
+                                    EXT_LENGTH is a string instead of an integer,
+                                    that string will be used for each new file. 
+                                    The default value is "txt".
+                                    """))
+    args = parser.parse_args()
+    if args.generate:
+        try:
+            numberOfFiles = int(args.generate[2]) + 1
+            for i in range(1, numberOfFiles):
+                run('touch {}'.format(gen_random_name(
+                    int(args.generate[0]), args.generate[1])), shell=True)
+        except ValueError as error:
+            sys.exit(error)
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
