fd596412ecd20e16951687aa56328f80fc3865c3
rockhazard <rockhazardz@gmail.ocm>
2017-07-03 00:42:17 -0400

    Extract gen_files_set function from parent function.

--- xtouch.py
+++ xtouch.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """
 ================================================================================
-Xtouch automates GNU touch by creating a given number of randomly-named files. 
+Xtouch automates GNU touch by creating a given number of randomly-named files.
 This is useful for creating mock files or creating and opening several new files
 for editing in other programs.
 
 Features:
 * Define filenames via a pattern: prefix.integer.suffix.extension.
 * Default pattern.
+* Increment filenames
 * Set case of generated files.
-* Pass standard gnu touch options to generated files.
 * Full use of gnu touch from xtouch.
 
 Written by Ike Davis
@@ -71,26 +71,25 @@ def increment_filename(zeroes, count):
     return file_count
 
 
-def gen_filename(prefix='/', randStrLen=8, suffix='/', ext="txt", sep='_', zeroes=5, count=5):
+def gen_filename(prefix='/', size=8, suffix='/', ext="txt", sep='_',
+                 zeroes=5, count=5):
     characters = string.printable[0:62]
-    randStrLenList = []
+    sizeList = []
     extList = []
 
+    # either use increment feature or use size to build random names
     if _options['increment']:
         filename = increment_filename(zeroes, count)
     else:
-        for i in range(1, int(randStrLen) + 1):
-            randStrLenList.append(random.choice(characters))
+        for i in range(1, int(size) + 1):
+            sizeList.append(random.choice(characters))
+        filename = "".join(sizeList)
     try:
         for i in range(1, int(ext) + 1):
             extList.append(random.choice(characters))
         extension = "".join(extList)
     except ValueError:
         extension = ext
-    if not filename:
-        filename = ''
-    elif randStrLenList:
-        filename = "".join(randStrLenList)
 
     # attach any prefix, suffix, and extension from arguments
     if prefix == '%':
@@ -113,47 +112,53 @@ def gen_filename(prefix='/', randStrLen=8, suffix='/', ext="txt", sep='_', zeroe
         return filename
 
 
+def gen_files_set(numberOfFiles, zeroes, default, match, sep):
+    # create set of unique filenames
+    if default:
+        return {gen_filename(zeroes, count=i)
+                for i in range(numberOfFiles)}
+    else:
+        return {gen_filename(match['prefix'], match['size'],
+                             match['suffix'],
+                             match['extension'], sep,
+                             zeroes, count=i)
+                for i in range(numberOfFiles)
+                }
+
+
 def file_factory(pattern='/.8./.txt', nFiles=1, sep='_', default=True):
     # use user-supplied arguments to produce unique files
     try:
         match = match_args(pattern)
         numberOfFiles = int(nFiles)
+        # avoid looping clobber
         maxFiles = int(''.join(['9' for i in range(int(match['size']))]))
         if _options['increment']:
             if numberOfFiles > maxFiles:
-                sys.exit('Error: requested number of files exceeds maximum.')
+                sys.exit(dedent("""\
+                                    Error: requested number of files exceeds
+                                    maximum. Increase 'int' amount."""))
 
-        # create set of unique filenames
-        def gen_files_set(numberOfFiles):
-            if default:
-                return {gen_filename(zeroes=str(match['size']), count=i)
-                        for i in range(numberOfFiles)}
-            else:
-                return {gen_filename(match['prefix'], match['size'],
-                                     match['suffix'],
-                                     match['extension'], sep,
-                                     zeroes=str(match['size']), count=i)
-                        for i in range(numberOfFiles)
-                        }
-
-        name = gen_files_set(numberOfFiles)
-        # build set until all filenames are unique
-        if len(name) != numberOfFiles:
+        names = gen_files_set(numberOfFiles, str(
+            match['size']), default, match, sep)
+        # build set until requested number of filenames in set
+        if len(names) != numberOfFiles:
             print('Building unique filenames...')
-            while len(name) < numberOfFiles:
-                diff = numberOfFiles - len(name)
-                remainder = gen_files_set(diff)
-                name = name.union(remainder)
+            while len(names) < numberOfFiles:
+                diff = numberOfFiles - len(names)
+                remainder = gen_files_set(
+                    diff, str(match['size']), default, match, sep)
+                names = names.union(remainder)
 
         # write files
         if _options['touch_options']:
             options = _options['touch_options']
         else:
             options = ''
-        name = list(name)
+        names = list(names)
         for i in range(numberOfFiles):
-            run('touch {} {}'.format(options, name[i]), shell=True)
-        print('Generated {} files.'.format(len(name)))
+            run('touch {} {}'.format(options, names[i]), shell=True)
+        print('Generated {} files.'.format(len(names)))
     except ValueError as error:
         sys.exit(error)
 
@@ -165,7 +170,7 @@ def main(*args):
     parser = argparse.ArgumentParser(
         prog='xtouch', prefix_chars='+',
         description=dedent("""\
-            %(prog)s is an automation wrapper for GNU touch. It creates a given 
+            %(prog)s is an automation wrapper for GNU touch. It creates a given
             number of files with either a user-defined or default pattern."""),
         epilog='Author: Ike Davis, distributed via the MIT license')
     parser.add_argument('options', nargs='*', metavar=('TOUCH_ARGS'),
@@ -178,11 +183,11 @@ def main(*args):
                         metavar=('PATTERN', 'NUMBER_OF_FILES'),
                         help=dedent("""\
                                     Create a NUMBER_OF_FILES according to a
-                                    PATTERN, in the form: 'str.int.str.str'. The 
+                                    PATTERN, in the form: 'str.int.str.str'. The
                                     'int' is required, but any other str may be
                                     '/' in order to exclude it. Example:
                                     'tmp_.2._work.log 1' may yield 'tmp_Yz_work.log'
-                                    where 'int' is an integer and produces a 
+                                    where 'int' is an integer and produces a
                                     random alphanumeric string of 'int' characters.
                                     Use '%%' to replace 'str' with a random dictionary
                                     word. Using 0 for 'int' requires that at least
@@ -190,7 +195,7 @@ def main(*args):
                                     """))
     parser.add_argument('++files', '+f', nargs='?', const=1, type=int,
                         metavar=('NUMBER_OF_FILES'),
-                        help=dedent("""Create a given NUMBER_OF_FILES in an 
+                        help=dedent("""Create a given NUMBER_OF_FILES in an
                             '8.txt' pattern, defaulting to one file."""))
     parser.add_argument('++increment', '+i', action='store_true',
                         help=dedent("""Increment filenames instead of generating
@@ -217,4 +222,4 @@ def main(*args):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    sys.exit(main(sys.argv[1:]))
